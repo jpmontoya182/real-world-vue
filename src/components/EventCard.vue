4e1d07f932eeb898331d99045b5d404cef453ced
jpmontoya182 <jpmontoya182@gmail.com>
2019-02-22 21:16:07 -0500

    Initial commit to repository

---
+++ src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="true" @click="handler(arg, event)"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: "I want to be single"
+    };
+  }
+};
+</script>
+
+<style></style>
